4cf6bbf5da793e4abd37a4404228dd3e25b9f540
mrathena <747779761@qq.com>
2017-08-08 08:33:37 +0800

    没什么

--- src/main/java/com/mrathena/transaction/service/TicketService.java
+++ src/main/java/com/mrathena/transaction/service/TicketService.java
@@ -5,7 +5,7 @@ public interface TicketService {
 	// 普通代码, 会有并发问题
 	boolean buyTicket(int ticketId) throws Exception;
 	
-	// 悲观锁方式
+	// 悲观锁方式, 会有性能问题
 	boolean buyTicket2(int ticketId) throws Exception;
 	
 	// 乐观锁方式
